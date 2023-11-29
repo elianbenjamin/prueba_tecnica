c948b82b8d7a5288fd2c39c0e5372184fb5bb8fe
elianbenjamin <elianbenjamin30@gmail.com>
2023-11-29 11:05:46 -0300

    bug fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,32 @@
 "use client";
 import React from 'react';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { People } from '@/data/people';
 
-export type HomeProps = {
-	// types...
-}
 
-const Home: React.FC<HomeProps>  = ({}) => {
+export interface HomeInterface {}
+
+const Home: React.FC<HomeInterface>  = () => {
+	const pageSize = 5;
+	const colums = [{
+		field: 'name',
+		headerName: 'Name',
+		flex: 1,
+		minWidth: 150,
+		renderCell: (params: GridRenderCellParams) => <>{params.field}</>
+
+	}]
 	return (
 		<div>
- 			Home works!
+ 			<DataGrid 
+			rows={People}
+			columns={colums}
+			disableColumnSelector
+			disableRowSelectionOnClick
+			autoHeight
+			pageSize={pageSize}
+			rowsPerPageOptions={[pageSize]}
+			/>
  		</div>
 	);
 };
